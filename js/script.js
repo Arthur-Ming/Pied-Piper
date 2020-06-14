e12ea80187033d94147ec2425cfaa17558a49b4a
Arthur-Ming <arthurming7@gmail.com>
2020-06-14 16:14:21 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock')
 	});
 });
+*/
+
+function active() {
+	document.querySelector('.header__menu').classList.toggle("active");
+	document.querySelector('.header__burger').classList.toggle("active");
+	document.body.classList.toggle("lock");
+}
+
+document.querySelector('.header__burger').onclick = active;
+document.querySelectorAll('.header__link')[0].onclick = active;
+document.querySelectorAll('.header__link')[1].onclick = active;
+document.querySelectorAll('.header__link')[2].onclick = active;
