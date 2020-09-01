44fe96c607a9f5f3fc13accc947f70c2f54bd21b
Arthur-Ming <arthurming7@gmail.com>
2020-09-01 08:00:35 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -3,9 +3,46 @@
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock')
 	});
-});
-*/
+});  */
 
+$(document).ready(function () {
+
+	$('.header__burger').click(function (event) {
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock')
+	});
+
+
+	$('.header__list').on('click', '.header__link', function () {
+		let $w = $('.header').height();
+		let el = $(this);
+		let dest = el.attr('href'); // получаем направление
+		if (dest !== undefined && dest !== '') { // проверяем существование
+			$('html').animate({
+				scrollTop: $(dest).offset().top - $w // прокручиваем страницу к требуемому элементу
+			}, 500 // скорость прокрутки
+			);
+		}
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock')
+
+		return false;
+	});
+
+	$('.header__logo').on('click', function () {
+
+		$('html').animate({
+			scrollTop: 0 // прокручиваем страницу к требуемому элементу
+		}, 500 // скорость прокрутки
+		);
+
+		return false;
+	});
+
+});
+
+
+/*
 function active() {
 	document.querySelector('.header__menu').classList.toggle("active");
 	document.querySelector('.header__burger').classList.toggle("active");
@@ -15,27 +52,28 @@ function active() {
 document.querySelector('.header__burger').onclick = active;
 document.querySelector('.header__list').onclick = function (event) {
 
-	event.preventDefault();
+   event.preventDefault();
+   if (event.target.className != 'header__link') return;
 
-	document.querySelector(event.target.getAttribute('href')).scrollIntoView({ block: "start", behavior: "smooth" });
+	let href = event.target.getAttribute('href');
+
+   if(href !== undefined && href !== '')
+		document.querySelector(href).scrollIntoView({ block: "start", behavior: "smooth" });
 
 	if (document.documentElement.clientWidth > 768) return;
 
-	if (event.target.className != 'header__link') return;
-
 	active();
 
-
-	
-}
+};
 
 document.querySelector('.header__logo').onclick = function (event) {
 
 	event.preventDefault();
 	document.querySelector("#h").scrollIntoView({ block: "start", behavior: "smooth" });
-	
+
 }
 
+*/
 
 /*document.querySelectorAll('.header__link')[0].onclick = active;
 document.querySelectorAll('.header__link')[1].onclick = active;
